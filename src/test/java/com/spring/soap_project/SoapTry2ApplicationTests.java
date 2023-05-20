21997153f5eeec4c0a58aa2beebe60587a29b807
Kirill <kirill.marchenko@praguecollege.cz>
2023-05-21 01:35:07 +0200

    Works

---
+++ src/test/java/com/spring/soap_project/SoapTry2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.soap_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapTry2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
